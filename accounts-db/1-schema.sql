6e7960914d80af360fcb04436ee1e5757f2dff43
Felipe Zumarraga <felipezumarraga@hotmail.com>
2025-04-16 14:36:11 -0500

    FZU: rename scripts for database initialization

---
+++ accounts-db/1-schema.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE IF NOT EXISTS accounts_db;
+
+USE accounts_db;
+
+CREATE TABLE IF NOT EXISTS accounts (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    account_number VARCHAR(50) NOT NULL,
+    type VARCHAR(50) NOT NULL,
+    initial_amount DECIMAL(15, 2) NOT NULL,
+    is_active BOOLEAN NOT NULL DEFAULT TRUE,
+    client_id INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS transactions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    tran_date DATETIME NOT NULL,
+    type VARCHAR(50),
+    amount DECIMAL(15, 2) NOT NULL,
+    balance DECIMAL(15, 2),
+    account_id INT NOT NULL,
+    FOREIGN KEY (account_id) REFERENCES accounts(id)
+);
